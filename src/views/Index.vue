1ba5236952830b0f9c820814d6fa72bd5525920d
JBumblebee <532458665@qq.com>
2019-10-20 13:55:33 +0800

    项目初始化

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="index">
+    初始化
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'index',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
